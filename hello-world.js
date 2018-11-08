64f674f7a05d6ddea44f9408930db50b79762a91
Celson Gutkowski <idkdavidc@gmail.com>
2018-11-08 15:28:45 -0500

    Finished button changes

--- hello-world.js
+++ hello-world.js
@@ -1,3 +1,13 @@
 function HelloConsole() {
-  console.log("Hello, World!")
+  console.log("Hello, World!");
+}
+function HelloPopup() {
+  window.alert("Hello, World!");
+}
+function HelloBrowser() {
+  document.getElementById('Hello_Browser').innerHTML = 'Hello, World!';
+}
+function HelloStranger() {
+  var retVal = prompt("Enter Your Name Please : ", "Your Name");
+  document.write("Welcome, " + retVal);
 }
